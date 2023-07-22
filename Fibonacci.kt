41e693573f12b2d4fdc108574a1993dccef77b48
Max <139705753+MaxonDeveloper@users.noreply.github.com>
2023-07-22 17:05:03 +0500

    From int to long

--- Fibonacci.kt
+++ Fibonacci.kt
@@ -1,13 +1,13 @@
-fun fibonacci(input: Int): Int {
+fun fibonacci(input: Int): Long {
     var n = 2
-    val list = mutableListOf(0, 1)
+    val list = mutableListOf<Long>(0, 1)
     when (input) {
         0 -> return 0
         1 -> return 1
-        else -> while (input != n) {
+        else -> while (input + 1 != n) {
             list.add(list[n - 1] + list[n - 2])
             n++
         }
     }
-    return list[input - 1]
+    return list[input]
 }
